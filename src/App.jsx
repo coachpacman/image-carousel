1a5c8a9c520dc9a63354abf1075c19befc13b076
Tim Lee <tim.wenyang.lee@gmail.com>
2023-11-24 23:59:12 -0500

    initial state

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imgSrc, setImgSrc] = useState([
+    './public/hari-panicker-2t28IxSTqF4-unsplash.jpg'
+  ])
 
   return (
     <>
       <h1>Image Carousel</h1>
       <div className="card">
         <FontAwesomeIcon className="chevron" icon={faChevronLeft} />
-        <img src='./public/hari-panicker-2t28IxSTqF4-unsplash.jpg'></img>
+        <img src={imgSrc[0]}></img>
         <FontAwesomeIcon className="chevron" icon={faChevronRight} />
       </div>
 
